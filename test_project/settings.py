872d48ea94178d85931a38cbe197900e26c68711
Denis Nikiforov <deshq.nikiforov@gmail.com>
2021-09-02 10:43:05 +0300

    Add Facebook registration

--- test_project/settings.py
+++ test_project/settings.py
@@ -44,7 +44,9 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    
     'allauth.socialaccount.providers.google',
+    'allauth.socialaccount.providers.facebook',
 
     'file_sharing',
 ]
