f80e2cb83d94ffd61139169a02932398280b95c5
Denis Nikiforov <deshq.nikiforov@gmail.com>
2021-09-22 10:26:47 +0300

    Changed user_directory_path for media

--- file_sharing/models.py
+++ file_sharing/models.py
@@ -10,10 +10,8 @@ User = get_user_model()
 
 # Create your models here.
 
-def user_directory_path(instance, filename):
-    # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
-    return 'user_{0}/{1}'.format(instance.user.id, filename)
-
+def user_directory_path(requests, filename):
+    return 'user_upload_files/{0}/{1}'.format(requests.user, filename)
 class FileManager(models.Manager):
 
     def get_queryset(self):
